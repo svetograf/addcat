03f4ca1c5012b481c6092dbb71475b8ec7452ada
sv <svetograf@gmail.com>
2023-05-03 21:32:55 +0300

    feat: remove unused menu items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Main', url: '/folder/inbox', icon: 'mail' },
+    { title: 'My cats', url: '/folder/favorites', icon: 'heart' },
+    { title: 'Account', url: '/folder/spam', icon: 'warning' },
   ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
 }
